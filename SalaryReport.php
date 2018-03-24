b9b94da54ac6bd1d37ff7dc8c50ebc5b48c77cf1
VoleyBob <voleybob@gmail.com>
2018-03-24 09:57:43 +0100

    Ćwiczenie 4. Interfacee

---
+++ SalaryReport.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * ĆW. 4. Ćwiczenie 4
+* Zaimplementuj interfejs RaportableInterface, który będzie zawierał 2 metody:
+* •	printPdf();
+* •	printCsv();
+* oraz stwórz dwie klasy:
+* •	SalaryReport
+* •	Exam
+*/
+
+class SalaryReport implements RaportableInterface {
+    function printPdf() {
+
+    }
+
+    function printCsv() {
+
+    }
+    
+};
+    
---
+++ cwiczenie_4_interface.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * ĆW. 4. Ćwiczenie 4
+* Zaimplementuj interfejs RaportableInterface, który będzie zawierał 2 metody:
+* •	printPdf();
+* •	printCsv();
+* oraz stwórz dwie klasy:
+* •	SalaryReport
+* •	Exam
+*/
+
